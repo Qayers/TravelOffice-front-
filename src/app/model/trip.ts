167766c9fd7b3c013e028bb1c6e978e997c26b2a
MKMichalski <mateuszmichalskiK@gmail.com>
2018-08-04 13:00:46 +0200

    added form to add trip

---
+++ src/app/model/trip.ts
@@ -0,0 +1,19 @@
+
+import { Time } from "@angular/common";
+
+export class Trip{
+id: number;
+city_to: number;
+count_of_days: String;
+count_of_person: String;
+departure_date: Time;
+description: String;
+price_for_adult:number;
+price_for_child: number;
+promotion: number;
+return_date: Time;
+id_airportfrom: number;
+id_airportto: number;
+id_hotel: number;
+
+}
