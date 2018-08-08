9bca5d5570ec06fb18df0e8b213bbbb931d2762b
Andrzej Kulesza <qayers.dev@gmail.com>
2018-08-08 17:29:01 +0200

    Add browser's result for Continent,country,city

--- src/app/trip.service.ts
+++ src/app/trip.service.ts
@@ -1,21 +1,21 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { HttpHeaders } from '@angular/common/http';
-import { TripSeries } from './model/TripSeries';
-import { Trip } from './model/trip';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {TripSeries} from './model/TripSeries';
+import {Trip} from './model/trip';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
- };
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class TripService {
 
-  public trips:TripSeries;
+  public trips: TripSeries;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   addHttpTrip(trip) {
     return this.http.post<Trip>('http://localhost:8080/addTrip', trip);
@@ -24,19 +24,61 @@ export class TripService {
   getHttpTrip() {
     return this.http.get<TripSeries>('http://localhost:8080/tripFrom');
   }
- 
- getTrip() {
-  this.getHttpTrip().subscribe(
-    data =>
-    {
-      this.trips=data;
-    },
-    err => console.error(err),
-    () => console.log('done loading trips')
-  );
-} 
 
+  getHttpTripCito_Id(id: number) {
+    return this.http.get<TripSeries>('http://localhost:8080/tripFindByCitiTo_Id/' + id);
+  }
 
+  getHttpTripCountry_Id(id: number) {
+    return this.http.get<TripSeries>('http://localhost:8080/tripFindByCountry_Id/' + id);
+  }
 
+  getHttpTripContinent_Id(id: number) {
+    return this.http.get<TripSeries>('http://localhost:8080/tripFindByContinent_Id/' + id);
+  }
+
+  getTrip() {
+    this.getHttpTrip().subscribe(
+      data => {
+        this.trips = data;
+      },
+      err => console.error(err),
+      () => console.log('done loading trips')
+    )
+  }
+
+  getTripFindByCityTo_Id(id: number) {
+    this.getHttpTripCito_Id(id).subscribe(
+      data => {
+        this.trips = data;
+      },
+      err => console.error(err),
+      () => console.log('done loading trips')
+    );
+  }
+
+  getTripFindByCountry_Id(id: number) {
+    this.getHttpTripCountry_Id(id).subscribe(
+      data => {
+        this.trips = data;
+      },
+      err => console.error(err),
+      () => console.log('done loading trips')
+    );
+  }
+
+  getTripFindByContinent_Id(id: number) {
+    this.getHttpTripContinent_Id(id).subscribe(
+      data => {
+        this.trips = data;
+      },
+      err => console.error(err),
+      () => console.log('done loading trips')
+    );
+  }
 
 }
+
+
+
+
